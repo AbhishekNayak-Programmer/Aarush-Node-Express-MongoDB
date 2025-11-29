9f6f4ff7a624cb03ba38a279b583df6ea4d3283b
Abhisheknayak-programer <sahiservice300@gmail.com>
2025-11-29 09:48:15 +0530

    Day 6 Completed

---
+++ Day_6/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Student = require("./model/students");
+const app = express();
+
+// Connecting with Database
+const dbName = "students";
+mongoose
+  .connect(`mongodb://localhost:27017/${dbName}`)
+  .then(() => console.log("Connected With Database"))
+  .catch((err) => console.log("Connection ERROR!!!"));
+
+app.use(express.json());
+
+// GET - GETTING SOMETHING
+// POST - CREATE
+// PUT - COMPLETE UPDATE
+// PATCH - PARTIALLY UPDATE SOME FIELDS
+// DELETE - COMPLETELY DELETE
+
+// Working with routes
+app.get("/students", async (req, res) => {
+  const studentsData = await Student.find();
+
+  res.status(200).json({
+    message: "Fetched Students",
+    students: studentsData,
+  });
+});
+
+app.post("/students", async (req, res) => {
+  let response = await Student.create({
+    name: req.body.name,
+    age: req.body.age,
+    email: req.body.email,
+  });
+
+  res.status(201).json({
+    message: "Student Created",
+    data: response,
+  });
+});
+
+const port = 8000;
+app.listen(port, () =>
+  console.log(`Server is Running at port http://localhost:${port}`)
+);
